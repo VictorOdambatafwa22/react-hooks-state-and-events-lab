d5f4eef56ec2183e8c0ab4f7d210501764279137
victor odambatafwa <omondivictoras@gmail.com>
2023-07-21 14:37:11 +0300

    completed assignment

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+const [isInCat,setIsInCat]=useState(false)
+const liClass=isInCat?"in-cart":""
+
+  function handleCat(){
+   setIsInCat(!isInCat)
+  }
+
   return (
-    <li className="">
+    <li className={liClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={handleCat} className="add">{isInCat?"Remove From Cart":"Add to Cart"}</button>
     </li>
   );
 }
